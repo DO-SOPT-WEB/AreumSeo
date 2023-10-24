5d60f5edde1bae35e767e5c0baf28f3c9f1a67f1
seoAreum <1971236@hansung.ac.kr>
2023-10-24 18:01:28 +0900

    feat: 스크롤 시 버튼 투명도 조절 구현

--- assignment/week_1/PhotoBooth/photoBooth.js
+++ assignment/week_1/PhotoBooth/photoBooth.js
@@ -1,6 +1,9 @@
 const prevLeftBtn = document.querySelector(".prevLeftBtn");
 const prevRightBtn = document.querySelector(".prevRightBtn");
 
+const goTopBtn = document.querySelector(".goTopBtn");
+const homeHeight = goTopBtn.getBoundingClientRect().height;
+
 // 좌측 끝으로 이동하는 기능
 prevLeftBtn.addEventListener("click", () => {
   const prevSection = document.querySelector("section#prevSection");
@@ -12,4 +15,12 @@ prevRightBtn.addEventListener("click", () => {
   const prevSection = document.querySelector("section#prevSection");
   // 최대 너비 값 구하기
   prevSection.scrollLeft = prevSection.scrollWidth - prevSection.clientWidth;
+});
+
+// 투명도 조절 기능
+document.addEventListener("scroll", () => {
+  // 수식을 활용하여 스크롤 시 투명도 값을 조절해줌
+  goTopBtn.style.opacity = window.scrollY / homeHeight / 50;
+
+  console.log(window.scrollY / homeHeight / 50);
 });
