203e29fb9c9833d25abdc746489367636e4e8981
seoAreum <1971236@hansung.ac.kr>
2023-10-25 17:40:52 +0900

    feat: 수입, 지출에 대한 상수 데이터 정의

---
+++ assignment/week_1/Account/account.js
@@ -0,0 +1,25 @@
+const HISTORY_LIST = [
+  {
+    category: "과외비",
+    place: "10월 월급",
+    history: 500000,
+  },
+  {
+    category: "식비",
+    place: "닭도리탕 문정역점",
+    history: -30000,
+  },
+  {
+    category: "용돈",
+    place: "용돈 획득",
+    history: 100000,
+  },
+  {
+    category: "식비",
+    place: "버거킹 햄버거 단품",
+    history: -10000,
+  },
+];
+
+// 최초 나의 자산
+let INIT_BALANCE = 0;
