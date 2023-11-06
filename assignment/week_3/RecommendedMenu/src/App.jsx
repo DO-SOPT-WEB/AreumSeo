04d9b2c9f110b8d390a09cab3fea049491a3f12d
seoAreum <1971236@hansung.ac.kr>
2023-11-06 23:43:30 +0900

    init: styled-components 활용 초기 세팅

--- assignment/week_3/RecommendedMenu/src/App.jsx
+++ assignment/week_3/RecommendedMenu/src/App.jsx
@@ -1,11 +1,15 @@
-import styled, {ThemeProvider} from 'styled-components';
-function App() {
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./style/GlobalStyle";
+import Header from "./components/header";
+import theme from "./style/theme";
 
+function App() {
   return (
     <ThemeProvider theme={theme}>
-      
+      <GlobalStyle />
+      <Header />
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
---
+++ assignment/week_3/RecommendedMenu/src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>어쩌고저쩌고</div>;
+};
+
+export default Header;
