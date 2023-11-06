521f8e1c39aa5999c3fe29b8d5e44264fdb517ee
seoAreum <1971236@hansung.ac.kr>
2023-11-06 23:24:00 +0900

    style: 스타일 기본 세팅

---
+++ assignment/week_3/RecommendedMenu/src/style/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+font-family: 'UhBeeSe_hyun';
+src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeeSe_hyun.woff') format('woff');
+font-weight: normal;
+font-style: normal;
+}
+
+body {
+    font-family: 'UhBeeSe_hyun';
+}
+`;
+
+export default GlobalStyle;
---
+++ assignment/week_3/RecommendedMenu/src/style/theme.js
@@ -0,0 +1,28 @@
+const colors = {
+  white: "#FFFFF0",
+  lightPink: "#FEE1E8",
+  lightPurple: "#ECD5E3",
+  purple: "#CBAACB",
+  lightYellow: "#FFFFB5",
+  yellow: "#FBDEA2",
+  darkPink: "#FF968A",
+  pink: "#E5B9B5",
+  mint: "#C6DBDA",
+  salmon: "#FCB9AA",
+  lightGreen: "#CCE2CB",
+  green: "#8EB695",
+  darkGreen: "#2D4849",
+  lightPink_modal: "#ffcad6",
+  darkPink_modal: "#d03c5a",
+};
+
+const font = {
+  fontFamily: "UhBeeSe_hyun",
+};
+
+const theme = {
+  colors,
+  font,
+};
+
+export default theme;
