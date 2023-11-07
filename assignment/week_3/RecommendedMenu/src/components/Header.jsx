41a44d8d447b4504b166919ec538dff031be3f7e
seoAreum <1971236@hansung.ac.kr>
2023-11-07 12:01:42 +0900

    feat: main header 생성

--- assignment/week_3/RecommendedMenu/src/components/Header.jsx
+++ assignment/week_3/RecommendedMenu/src/components/Header.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <div>어쩌고저쩌고</div>;
+  return (
+    <St.MainHeader>
+      <h1>🍥 오늘의 점메추 🍥</h1>
+    </St.MainHeader>
+  );
+};
+
+const St = {
+  MainHeader: styled.header`
+    padding: 1.5rem 0;
+    text-align: center;
+
+    background-color: ${({ theme }) => theme.colors.green};
+    color: ${({ theme }) => theme.colors.white};
+  `,
 };
 
 export default Header;
