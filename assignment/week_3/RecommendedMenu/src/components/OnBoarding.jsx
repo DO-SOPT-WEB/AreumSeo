481ee6f7dd02eedc595b9f7b6b4a315c2c90c1b0
seoAreum <1971236@hansung.ac.kr>
2023-11-08 14:26:23 +0900

    feat: 단계 별 헤더 문구 변경

--- assignment/week_3/RecommendedMenu/src/components/OnBoarding.jsx
+++ assignment/week_3/RecommendedMenu/src/components/OnBoarding.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import StartRecommend from "./StartRecommend";
 import SelectCategory from "./SelectCategory";
 
 const OnBoarding = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [step, setStep] = useState(0);
+
+  useEffect(() => {
+    setStep(0);
+  }, []);
 
   return (
     <St.Container>
       <St.SelectCategoryHeader>
-        <h2>원하는 추천 방식을 골라줘 !</h2>
+        {step === 0 && <h2>원하는 추천 방식을 골라줘 !</h2>}
+        {step === 1 && <h2>오늘은 어떤 종류가 먹고 싶어?</h2>}
+        {step === 2 && <h2>그럼 이 중에는 뭐가 끌려?</h2>}
+        {step === 3 && <h2>마지막으로 골라줘 !</h2>}
+        {step === 4 && <h2>오늘의 추천 음식은 바로 !!</h2>}
       </St.SelectCategoryHeader>
 
       {selectedCategory ? (
-        <StartRecommend selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <StartRecommend
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          setStep={setStep}
+          step={step}
+        />
       ) : (
-        <SelectCategory setSelectedCategory={setSelectedCategory} />
+        <SelectCategory
+          setSelectedCategory={setSelectedCategory}
+          setStep={setStep}
+        />
       )}
     </St.Container>
   );
