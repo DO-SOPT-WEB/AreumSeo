efedf5f8226cc545191f1308d0f30134ad5f1756
seoAreum <1971236@hansung.ac.kr>
2023-11-09 17:51:20 +0900

    feat: 취향대로 추천 컴포넌트 초기 구현

--- assignment/week_3/RecommendedMenu/src/components/TasteRecommendation.jsx
+++ assignment/week_3/RecommendedMenu/src/components/TasteRecommendation.jsx
@@ -1,9 +1,18 @@
-const TasteRecommendation = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import ShowSelectedCategory from "./ShowSelectedCategory";
+import Step1 from "./Step1";
+
+const TasteRecommendation = (props) => {
+  const step = props.step;
+  const category = "취향대로 추천";
+
+  return step === 0 ? (
+    <ShowSelectedCategory
+      category={category}
+      setIsStartClicked={props.setIsStartClicked}
+    />
+  ) : (
+    <Step1 setStep={props.setStep} step={step} setIsStartClicked={props.setIsStartClicked}/>
+  );
 };
 
 export default TasteRecommendation;
