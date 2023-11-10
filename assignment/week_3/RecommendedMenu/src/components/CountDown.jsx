baea08f7d8d83e98748d33222786cba2f86bec37
seoAreum <1971236@hansung.ac.kr>
2023-11-10 21:29:33 +0900

    feat: 카운트 다운 렌더링 최적화

--- assignment/week_3/RecommendedMenu/src/components/CountDown.jsx
+++ assignment/week_3/RecommendedMenu/src/components/CountDown.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { useEffect, useRef, useReducer } from "react";
 import Recommendation from "./Recommendation";
-import { useEffect, useState, useRef } from "react";
+import { reducer } from "../libs/reducer";
 
 const CountDown = (props) => {
   const category = props.category;
-  const [time, setTime] = useState(3);
   const INTERVAL = 1000;
+  const [timerState, dispatch] = useReducer(reducer, {
+    time: 3,
+    category: "랜덤 추천",
+  });
 
   // useRef(): setState 값이 변하면 렌더링이 발생함. useRef의 current를 사용하면 current 값이 변하더라도 리렌더링되지 않기 때문에 렌더링 최적화를 이룰 수 있음.
   const timer = useRef(0);
@@ -13,20 +17,20 @@ const CountDown = (props) => {
   // setTimeout(): 두 번째 인자로 전달받은 시간이 지나면 한 번만 동작, 타이머 만료 후 첫 번째 인자로 전달받은 콜백함수를 호출함.
   // setInterval(): 두 번째 인자로 전달받은 시간을 간격으로 동작, 첫 번째 인자인 콜백함수가 시간 간격을 두고 반복적으로 호출됨.
   useEffect(() => {
-    if (time > 0) {
+    if (timerState.time > 0) {
       timer.current = setInterval(() => {
-        setTime(time - 1);
+        dispatch({ type: "SET_TIME", payload: 1 });
       }, INTERVAL);
     }
 
     return () => {
       clearInterval(timer.current);
     };
-  }, [time]);
+  }, [timerState.time]);
 
-  return time ? (
+  return timerState.time ? (
     <St.CountContainer>
-      <St.Count>{time}</St.Count>
+      <St.Count>{timerState.time}</St.Count>
     </St.CountContainer>
   ) : (
     <Recommendation
