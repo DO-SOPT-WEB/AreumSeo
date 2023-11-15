862afecc769ae746e497e84639a7441f6c5a5fab
seoAreum <1971236@hansung.ac.kr>
2023-11-15 19:38:01 +0900

    style: 색상 추가

--- assignment/week_4/login-signup/src/style/theme.js
+++ assignment/week_4/login-signup/src/style/theme.js
@@ -1,4 +1,5 @@
 const colors = {
+  black: "#000",
   white: "#FFFFF0",
   lightGray: "#D3D3D3",
   lightPurple: "#ECD5E3",
@@ -6,6 +7,8 @@ const colors = {
   lightYellow: "#FFFFB5",
   salmon: "#fec086",
   lightGreen: "#CCE2CB",
+  green: "#8EB695",
+  darkGreen: "#2D4849",
   lightPink: "#ffcad6",
   darkPink: "#d03c5a",
   lightBlue: "#A9E0EB",
