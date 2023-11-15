08daf7106f912f572972583abf9cf4a8554f5b92
seoAreum <1971236@hansung.ac.kr>
2023-11-15 15:44:32 +0900

    feat: 로그인 화면 퍼블리싱

--- assignment/week_4/login-signup/src/pages/LoginPage.jsx
+++ assignment/week_4/login-signup/src/pages/LoginPage.jsx
@@ -1,6 +1,86 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+
 // 로그인 페이지
 const LoginPage = () => {
-  return <div>로그인</div>;
+  const INPUT_CONTENTS = [
+    { category: "ID", placeholder: "아이디를 입력해주세요." },
+    { category: "PASSWORD", placeholder: "비밀번호를 입력해주세요." },
+  ];
+
+  const navigator = useNavigate();
+  const handleClickSignupBtn = () => {
+    navigator("/signup");
+  };
+
+  return (
+    <St.LoginSection>
+      <Header headerContents={"LOGIN"} />
+
+      {INPUT_CONTENTS.map((content, idx) => {
+        return (
+          <St.InputContainer key={idx}>
+            <St.InputCategory>{content.category}</St.InputCategory>
+            <St.InputContents
+              placeholder={content.placeholder}
+            ></St.InputContents>
+          </St.InputContainer>
+        );
+      })}
+
+      <St.ButtonContainer>
+        <St.LoginBtn>로그인</St.LoginBtn>
+        <St.SignUpBtn onClick={handleClickSignupBtn}>회원가입</St.SignUpBtn>
+      </St.ButtonContainer>
+    </St.LoginSection>
+  );
+};
+
+const St = {
+  LoginSection: styled.section`
+    gap: 0.5rem;
+  `,
+  InputContainer: styled.article`
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+    width: 100%;
+
+    gap: 2rem;
+  `,
+  InputCategory: styled.p`
+    font-size: 1.3rem;
+  `,
+  InputContents: styled.input`
+    padding: 0.5rem 3rem 0.5rem 0.3rem;
+  `,
+
+  ButtonContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    margin-top: 1rem;
+
+    gap: 0.5rem;
+  `,
+
+  LoginBtn: styled.button`
+    padding: 0.5rem 0;
+
+    background-color: ${({ theme }) => theme.colors.white};
+    color: ${({ theme }) => theme.colors.darkPink};
+    font-size: 1.2rem;
+  `,
+
+  SignUpBtn: styled.button`
+    padding: 0.5rem 0;
+
+    background-color: ${({ theme }) => theme.colors.darkPink};
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 1.2rem;
+  `,
 };
 
 export default LoginPage;
