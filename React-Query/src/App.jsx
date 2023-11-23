460046eae69438ead6566917cf9461fe72919c9d
seoAreum <1971236@hansung.ac.kr>
2023-11-24 03:17:45 +0900

    feat: 심심스 실습

---
+++ React-Query/src/App.jsx
@@ -0,0 +1,35 @@
+// App.jsx
+import { useState } from "react";
+import styled from "styled-components";
+import MyPage from "./MyPage";
+
+function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <Wrapper>
+      {isOpen ? (
+        <MyPage setOpen={setOpen} />
+      ) : (
+        <Button
+          onClick={() => {
+            setOpen(true);
+          }}>
+          마이페이지로 이동하기
+        </Button>
+      )}
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+const Button = styled.button``;
---
+++ Recoil/src/MyPage.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+// 유저에 따른 마이페이지
+const MyPage = ({ setOpen }) => {
+  const [response, setResponse] = useState("");
+
+  // 유저 정보를 get 하는 함수
+  const getUserInfo = async () => {
+    try {
+      const res = await axios.get("http://3.39.54.196/api/v1/members/300");
+      setResponse(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClickLogoutBtn = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <St.UserDetailSection>
+      <St.UserDetailArticle>
+        <St.UserInfoContainer>
+          <St.UserInfo>ID: {response.username}</St.UserInfo>
+          <St.UserInfo>NICKNAME: {response.nickname}</St.UserInfo>
+        </St.UserInfoContainer>
+      </St.UserDetailArticle>
+
+      <St.LogoutBtn onClick={handleClickLogoutBtn}>로그아웃</St.LogoutBtn>
+    </St.UserDetailSection>
+  );
+};
+
+const St = {
+  UserDetailSection: styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    padding: 2rem 3rem;
+    border-radius: 2rem;
+
+    background-color: #cf576b;
+  `,
+  UserDetailArticle: styled.article`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    gap: 1rem;
+  `,
+  UserImg: styled.img`
+    width: 13rem;
+    height: 11rem;
+  `,
+  UserInfoContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    gap: 1.5rem;
+  `,
+  UserInfo: styled.p`
+    padding: 0.5rem 1.5rem;
+    border-radius: 1rem;
+
+    color: #cf576b;
+    background-color: white;
+    font-size: 1.2rem;
+  `,
+  LogoutBtn: styled.button`
+    margin: 1rem 0 0.5rem;
+    padding: 0.5rem 1.5rem;
+    border-radius: 1rem;
+    border: 0.3rem solid white;
+
+    background-color: #cf576b;
+    color: white;
+
+    font-size: 1.2rem;
+
+    &:hover {
+      background-color: white;
+      color: #cf576b;
+    }
+  `,
+};
+
+export default MyPage;
